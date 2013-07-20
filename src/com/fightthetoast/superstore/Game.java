6c8744bf5f6ed5a0895f17a0a258ed25c945b5f8
UnacceptableUse <unacceptableuse@live.co.uk>
2013-07-20 22:23:44 +0100

    Added loading screen graphics, but bug in lwjgl API holding back progress Change-Id: Ib644693ed531c1bcb0d5bf6f8bc7dbcde3032072

--- src/com/fightthetoast/superstore/Game.java
+++ src/com/fightthetoast/superstore/Game.java
@@ -1,6 +1,10 @@
 package com.fightthetoast.superstore;
 
 import org.isjaki.GameBase;
+import org.isjaki.Loader;
+import org.isjaki.graphics.Texture;
+import org.isjaki.util.vec2f;
+import org.lwjgl.util.vector.Vector2f;
 
 import com.fightthetoast.superstore.screen.Screen;
 
@@ -11,6 +15,9 @@ public class Game extends GameBase{
 	public static final String GAME_TITLE = "Superstore Tycoon";
 	
 	protected Screen currentScreen = null;
+	
+	
+	Texture splash = Loader.texture("C:\\Users\\Peter\\git\\SuperstoreTycoon\\res\\loading.png");
 	
 	
 	public Game(String name, int width, int height) {
@@ -30,11 +37,19 @@ public class Game extends GameBase{
 		
 	}
 	
+	@Override
+	protected void update()
+	{
+		
+	}
+	
+	
 	
 	@Override
 	protected void render()
 	{
-		currentScreen.render();
+		splash.render(new vec2f(10, 20));
+		//currentScreen.render();
 	}
 	
 	
