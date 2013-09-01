51c3213c508179d6c0f0d96d23258669e41b2a18
UnacceptableUse <unacceptableuse@gmail.com>
2013-09-01 11:50:18 +0100

    Transitioning to Slick2D

--- src/com/fightthetoast/superstore/Game.java
+++ src/com/fightthetoast/superstore/Game.java
@@ -13,19 +13,22 @@ import static org.lwjgl.opengl.GL11.glMatrixMode;
 import static org.lwjgl.opengl.GL11.glOrtho;
 import static org.lwjgl.opengl.GL11.glViewport;
 
-import org.isjaki.GameBase;
-import org.isjaki.Isjaki;
-import org.isjaki.graphics.Color;
-import org.isjaki.util.vec2f;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.Sys;
+import org.lwjgl.input.Cursor;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.opengl.ImageData;
 
 import com.fightthetoast.superstore.screen.Screen;
 import com.fightthetoast.superstore.screen.ScreenInGame;
+import com.fightthetoast.superstore.screen.ScreenMainSplash;
 
-public class Game extends GameBase{
+public class Game implements org.newdawn.slick.Game{
 	
 
 	public static final int DEFAULT_WIDTH = 800, DEFAULT_HEIGHT = 600;
@@ -37,102 +40,23 @@ public class Game extends GameBase{
 	int fps;
 	int solidFPS;
 	long lastFPS;
+	public GameContainer container = new GameContainer(this);
 	
 
-	private Color clearColor = new Color(27, 18, 23);
-	
-	public Game(String name, int width, int height) {
-		super(name, width, height);
-		start();
-		
-	}
-	
-	@Override
-	public void start()
+
+	public Game()
 	{
-		try {
-			Display.setDisplayMode(new DisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT));
-			Display.setFullscreen(isFullscreen);
-			Display.setResizable(false);
-			Display.setTitle(SCREEN_NAME);
-			Display.setVSyncEnabled(false);
-			
-			Display.create();
-			Isjaki.create(Isjaki.INIT_EVERYTHING);
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(0);
-		}
-		
-		init2DGL();
-		init();
-		
-		while (!Display.isCloseRequested()) {
-			getDelta();
-			update();
-			render();
-			Display.update();
-		//	Display.sync(60); // cap fps to 60fps
-		}
-		Display.destroy();
+		this.init();
 	}
-	
-	private void init2DGL() {
-		glMatrixMode(GL_PROJECTION); {
-			glOrtho(0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, -1, 1);
-			glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-		}
 
-		glMatrixMode(GL_MODELVIEW); {
-			glClearColor(clearColor.r,
-					clearColor.g, clearColor.b, 1);
 
-			glEnable(GL_BLEND);
-			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-		}
-		
-		glLoadIdentity();
-	}
 
 	
 	public static void main(String[] args)
 	{
-		new Game(GAME_TITLE, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		new Game();
 	}
-	
-	@Override
-	protected void init()
-	{
-		System.out.println("Loading textures...");
-		
-		Resources.init();
-/*		setScreen(new ScreenMainSplash(this));
-		render();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {}*/
-		System.out.println("Done!");
-		setScreen(new ScreenInGame(this));
-		
-		
-	}
-	
-	@Override
-	protected void update()
-	{
-		currentScreen.tick();
-	}	
-	
 
-	
-	
-	
-	@Override
-	protected void render()
-	{
-		currentScreen.render();
-		updateFPS();
-	}
 	
 	
 	protected void setScreen(Screen screen)
@@ -141,32 +65,56 @@ public class Game extends GameBase{
 		currentScreen.onScreenDestroy(screen);
 		screen.onScreenCreate(currentScreen);
 		currentScreen = screen;
-		render();
+		//render();
 
 	}
-	public long getTime() {
-	    return System.nanoTime() / 1000000;
-	}
+
 	
-	public int getDelta() {
-	    long time = getTime();
-	    int delta = (int) (time - lastFrame);
-	    lastFrame = time;
-	 
-	    return delta;
-	}
 	
-	public void updateFPS() {
-		if (getTime() - lastFPS > 1000) {
-			solidFPS = fps;
-			fps = 0;
-			lastFPS += 1000;
-		}
-		Resources.font.render(solidFPS+" FPS.", new vec2f(1,1));
-		fps++;
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		System.out.println("Loading textures...");
 		
+		Resources.init();
+		setScreen(new ScreenMainSplash(this));
+		//render(container);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {}
+		System.out.println("Done!");
+		setScreen(new ScreenInGame(this));
 		
-	
+	}
+
+
+	@Override
+	public void update(GameContainer container, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void render(GameContainer container, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public boolean closeRequested() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
