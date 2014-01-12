3ba115b1834ec4e7a002f5a63d576299ff7ce1d5
Joel Margolis <jmargolis97@gmail.com>
2014-01-11 20:10:50 -0500

    formatting and thoughts and not sure. brain mush.

--- src/com/unacceptableuse/screen/ScreenShop.java
+++ src/com/unacceptableuse/screen/ScreenShop.java
@@ -30,20 +30,21 @@ public class ScreenShop extends Screen
 			for(int x = 0; x < floorLength; x++)
 			{
 				if(x == entranceX && y == entranceY)
-					ImageResource.tileEntrance.draw(350+(y-y*30+(x*29)), 200+(y*15+(x*15)));
+				{
+					ImageResource.tileEntrance.draw(350 + (y - y * 30 + (x * 29)), 200 + (y * 15 + (x * 15)));
+				}
 				else
-					ImageResource.tile.draw(350+(y-y*30+(x*29)), 200+(y*15+(x*15)));
-				//Game.drawText(x+", "+y, 360+(y-y*30+(x*29)), 205+(y*15+(x*15)));
+				{
+					ImageResource.tile.draw(350 + (y - y * 30 + (x * 29)), 200 + (y * 15 + (x * 15)));
+					Game.drawText(x + ", " + y, 360 + (y - y * 30 + (x * 29)), 205 + (y * 15 + (x * 15)));
+				}
 			}
 		}
 		
-		
-		
 		ImageResource.vendingMachineF.draw(175, 196);
 
-
 	}
-
+	
 	@Override
 	public void tick()
 	{
@@ -56,6 +57,7 @@ public class ScreenShop extends Screen
 		{
 			for(int x = 0; x < floorLength; x++)
 			{
+				tiles[x][y] = new Tile();
 				tiles[x][y] = new Tile();
 			}
 		}
